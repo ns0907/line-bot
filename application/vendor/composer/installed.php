ddbb473a396a4e8b676789da568bdf8bf2a0a248
nakahira <nakahira.shota@unizonbex.com>
2020-12-21 15:06:30 +0900

    composer install

---
+++ application/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0f65cab8ab12658d894b806581bc5b7e98daa140',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f65cab8ab12658d894b806581bc5b7e98daa140',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '1.6.0',
+      'version' => '1.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd827436f59d0081ce4e1e49ede4861648f8a0bca',
+    ),
+  ),
+);
